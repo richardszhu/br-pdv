c58bd56d420c4cfbc479ccd9b5b8b14370889ce2
richardszhu <richardzhu_sd@yahoo.com>
2019-10-26 23:14:27 -0700

    f

--- vis.py
+++ vis.py
@@ -6,6 +6,7 @@ Installation instructions: https://matplotlib.org/users/installing.html
 import matplotlib.pyplot as plt
 from portfolio import *
 
+
 def vis_pie(p, attr):
     """
     p is a Portfolio instance
@@ -14,17 +15,30 @@ def vis_pie(p, attr):
     counts = p.get_counts(attr)
 
     fig1, ax1 = plt.subplots()
-    ax1.pie(list(counts.values()), explode=[0] * len(counts), labels=tuple(counts), autopct='%1.1f%%', shadow=False, startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    ax1.set_title("Diversity of Portfolio: {} of Investments".format(attr_to_english(attr)))
-    ax1.legend(["{0}: ${1}.00".format(key, counts[key]) for key in list(counts.keys())],title="Value", bbox_to_anchor=(1,0), loc="center right",
-                          bbox_transform=plt.gcf().transFigure)
+    ax1.pie(list(counts.values()),
+            explode=[0] * len(counts), 
+            labels=tuple(counts),
+            autopct='%1.1f%%',
+            shadow=False,
+            startangle=90)
+    # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax1.axis('equal')
+    ax1.set_title("Diversity of Portfolio: {} of Investments".format(
+        attr_to_english(attr)))
+    ax1.legend(["{0}: ${1}.00".format(key, counts[key]) for key in list(counts.keys())],
+               title="Value",
+               bbox_to_anchor=(1, 0),
+               loc="center right",
+               bbox_transform=plt.gcf().transFigure)
     plt.subplots_adjust(left=0.0, bottom=0.1, right=0.45)
+
     plt.show()
 
 
+english_attrs = {"exchangeAcronym": "Exchange", "issFtse1Industry": "Industry",
+                 "issFtse3Sector": "Sector",  "assetType": "Asset Type"}
 
-english_attrs = {"exchangeAcronym" : "Exchange", "issFtse1Industry" : "Industry", "issFtse3Sector" : "Sector" ,  "assetType": "Asset Type"}
+
 def attr_to_english(attr):
     return english_attrs.get(attr, attr.capitalize())
 
