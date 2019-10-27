77422eab90733879c17ca6b284233614c5ef14b3
richardszhu <richardzhu_sd@yahoo.com>
2019-10-26 22:51:54 -0700

    Added a cleaner ipynb "Gui"

--- vis.py
+++ vis.py
@@ -13,17 +13,11 @@ def vis_pie(p, attr):
     """
     counts = p.get_counts(attr)
 
-    tup = tuple(counts)
-    explode = [0] * len(counts)
-
     fig1, ax1 = plt.subplots()
-    ax1.pie(list(counts.values()), explode=explode, labels=tup, autopct='%1.1f%%', shadow=False, startangle=90)
+    ax1.pie(list(counts.values()), explode=[0] * len(counts), labels=tuple(counts), autopct='%1.1f%%', shadow=False, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     ax1.set_title("Diversity of Portfolio: {} of Investments".format(attr_to_english(attr)))
-    ax1.legend (
-            ["{0}: ${1}.00".format(key, counts[key]) for key in list(counts.keys())],
-            title="Value"
-            )
+    ax1.legend (["{0}: ${1}.00".format(key, counts[key]) for key in list(counts.keys())],title="Value")
     plt.show()
 
 
@@ -32,4 +26,4 @@ english_attrs = {"exchangeAcronym" : "Exchange", "issFtse1Industry" : "Industry"
 def attr_to_english(attr):
     return english_attrs.get(attr, attr.capitalize())
 
-vis_pie(Portfolio(["aapl", 'MsFt', 'RKUNY'], [1000, 500, 1200]), "country")
+#vis_pie(Portfolio(["aapl", 'MsFt', 'RKUNY'], [1000, 500, 1200]), "country")
