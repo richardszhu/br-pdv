1b1ee59cacc6b2d13ec9ac00baf6d5fac571de86
richardszhu <richardzhu_sd@yahoo.com>
2019-10-27 03:19:01 -0700

    almost done

--- vis2.py
+++ vis2.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from portfolio import *
 from matplotlib.widgets import TextBox
 
+english_attrs = {"exchangeAcronym": "Exchange", "issFtse1Industry": "Industry",
+                 "issFtse3Sector": "Sector",  "assetType": "Asset Type"}
+
 def vis_pie(p, attr):
     """
     p is a Portfolio instance
@@ -16,23 +19,16 @@ def vis_pie(p, attr):
 
     fig1, ax1 = plt.subplots()
     ax1.pie(list(counts.values()),
-            explode=[0] * len(counts), 
+            explode=[0] * len(counts),
             labels=tuple(counts),
             autopct='%1.1f%%',
             shadow=False,
             startangle=90)
     # Equal aspect ratio ensures that pie is drawn as a circle.
     ax1.axis('equal')
-    ax1.set_title("Diversity of Portfolio: {} of Investments".format(
-        attr_to_english(attr)))
-    ax1.legend(["{0}: ${1}.00".format(key, counts[key]) for key in list(counts.keys())],
-               title="Value",)
+    ax1.set_title("Diversity of Portfolio: {} of Investments".format(attr_to_english(attr)))
+    ax1.legend(["{0}: ${1}.00".format(key, counts[key]) for key in list(counts.keys())], title="Value",)
     plt.show()
-
-
-english_attrs = {"exchangeAcronym": "Exchange", "issFtse1Industry": "Industry",
-                 "issFtse3Sector": "Sector",  "assetType": "Asset Type"}
-
 
 def attr_to_english(attr):
     return english_attrs.get(attr, attr.capitalize())
@@ -45,6 +41,6 @@ def submit(input_string):
     vis_pie(Portfolio(ticker_list, price_list), req_attr)
 
 axbox = plt.axes([0.1, 0.05, 0.8, 0.075])
-text_box = TextBox(axbox, 'Ticker', initial='googl aapl, 300 500, country')
+text_box = TextBox(axbox, 'Ticker', initial='BLK GOOGL RKUNY BABA, 1200 100 400 500, country')
 text_box.on_submit(submit)
 plt.show()
