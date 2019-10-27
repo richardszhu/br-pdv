ad1191be63f4aea1920e2205d7ccbed95038c1a4
richardszhu <richardzhu_sd@yahoo.com>
2019-10-26 17:45:21 -0700

    made the attribute count method more general

--- br.py
+++ br.py
@@ -10,8 +10,14 @@ x = portfolioAnalysisRequest.text # get in text string format
 portfolio = portfolioAnalysisRequest.json # get as json object
 p_stocks = portfolio()['resultMap']["SECURITY"]
 
-country_count = {}
-for stock in p_stocks:
-    c = stock["country"]
-    country_count[c] = 1 + country_count.get(c, 0)
-print(country_count)
+def get_counts(attr_string):
+    """
+    returns dictionary, (Attribute, Count)
+    """
+    a_counts = {}
+    for stock in p_stocks:
+        c = stock[attr_string]
+        a_counts[c] = 1 + a_counts.get(c, 0)
+    return a_counts
+
+print(get_counts("country"))
