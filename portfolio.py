74d42faaf4139a83ae7d7d421e36c505291d153c
rszhu <richardzhu_sd@yahoo.com>
2020-06-11 18:26:45 -0700

    Made more Pythonic

--- portfolio.py
+++ portfolio.py
@@ -7,56 +7,63 @@ Utilizing BlackRock's Security Data API
 """
 import requests
 from operator import itemgetter
+from collections import defaultdict
 
 class Portfolio():
     """
     Instance vars:
         params - passed into the API requests
-        p - a json of the portfolio
-        p_stocks - the list of stocks in the portfolio, and which ones are
+        p_json - a json of the portfolio
+        p_stocks - the list of stocks in the portfolio
         values - list of worth of corresponding stock is in the portfolio
-     """
+    """
 
     def __init__(self, tickers_list, values=[]):
         """
-        tickers_list is a list of strings, each string being a ticker
-
-        Takes list of tickers and converts it into a string that can be passed into the API
-        Example of passable string: "ticker:AAPL,ticker:MSFT,ticker:RKUNY"
+        Args:
+            tickers_list: List of ticker strings
+            values: List of corresponding weights (dollars) for each ticker
         """
-        if values:
-            self.values = values
-        else:
-            self.values = [1 for _ in tickers_list]
+        
+        self.values = values if values else [1] * len(tickers_list)
 
         #https://stackoverflow.com/a/13668413 Needed to keep order in sync with API
         self.tickers_list, self.values = [list(x) for x in zip(*sorted(zip(tickers_list, self.values), key=itemgetter(0)))]
 
-        self.params = los_to_brparams(self.tickers_list)
+        portfolio_analysis_request = requests.get("https://www.blackrock.com/tools/hackathon/security-data", 
+            params={'identifiers': format_tickers_to_Aladdin(self.tickers_list)})
 
-        portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/security-data", params={'identifiers': self.params})
-        self.p = portfolioAnalysisRequest.json  # get as json object
-        self.p_stocks = (self.p)()['resultMap']["SECURITY"][:len(tickers_list)] # cut off "duplicate" stocks that share the same ticker
+        self.p_json = portfolio_analysis_request.json
+        self.p_stocks = (self.p_json)()['resultMap']["SECURITY"][:len(tickers_list)]
 
+    
     def get_counts(self, attr):
         """
-        Returns dictionary: {Security Attribute: Count}
+        Args:
+            attr: The specific attribute we're counting values of.
 
-        Security attribute is stuff like:
-        country, currency, exchangeAcronym, issFtse1Industry, issFtse3Sector, assetType
-        All attributes: https://www.blackrock.com/tools/api-tester/hackathon?apiType=securityData
+        Returns:
+            Dictionary - {Security Attribute: Count}
         """
+        counts = defaultdict(int)
+        for stock, value in zip(self.p_stocks, self.values):
+            counts[stock.get(attr, "No " + attr)] += value
+        return counts
 
-        a_counts = {}
-        for i in range(len(self.p_stocks)):
-            c = self.p_stocks[i].get(attr, "No {}".format(attr))
-            a_counts[c] = self.values[i] + a_counts.get(c, 0)
-        return a_counts
+        #a_counts = {}
+        #for i in range(len(self.p_stocks)):
+        #    c = self.p_stocks[i].get(attr, "No {}".format(attr))
+        #    a_counts[c] = self.values[i] + a_counts.get(c, 0)
+        #return a_counts
 
-def los_to_brparams(los):
-    params = ''
-    for ticker in los:
-        params += "ticker:{},".format(ticker.upper()) #tickers have to be uppercase
-    return params[:-1]  # cut off the last comma
 
-#print (Portfolio(['SPY', 'RKUNY'], [300, 500]).get_counts("assetType"))
+def format_tickers_to_Aladdin(tickers):
+    """
+    Args:
+        tickers: A list of stock ticker strings.
+    
+    Returns:
+        The joined string of tickers, correctly formatted to pass into the BlackRock API.
+        Example of correctly formatted string: "ticker:AAPL,ticker:MSFT,ticker:RKUNY"
+    """
+    return ",".join(["ticker:" + t.upper() for t in tickers])
